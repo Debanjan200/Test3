12e76f5c20db424f51f423b498ee9b2bf4bb11ca
Debanjan <debanjan@LAPTOP-6VO262K2>
2023-08-14 00:07:55 +0530

    changing MyTest.cpp and adding Thread

--- MyTest.cpp
+++ MyTest.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <thread>
 
 void magic(){
-    int sum=0;
+    int sum=1;
     for(int i=0;i<10;i++){
-        sum+=i;
+        sum*=i;
     }
 
     std::cout<<sum<<"\n";
 }
 
 int main(){
-    magic();
+    std::thread t1(magic);
+    t1.join();
 }
